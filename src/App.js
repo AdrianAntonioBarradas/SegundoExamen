a9b895f83755f93c62605427f1181c46b3dceda2
AdrianAntonioBarradas <98973390+AdrianAntonioBarradas@users.noreply.github.com>
2022-05-29 21:57:16 -0500

    implementando Photo COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Table from "./components/Table";
 import "./styles.css";
 
 export default function App() {
+  const [url, setUrl] = useState();
+  const [json, setJson] = useState();
+  const [table, showTable] = useState(0);
+  const [photo, showPhoto] = useState(0);
+  // prueba jsno
+  const getConsulta = async (currentUrl) => {
+    try {
+      let formData = new FormData();
+      const url = currentUrl;
+      let result = await axios({
+        url,
+        method: "GET",
+        dataType: "json",
+        ContentType: "application/json",
+        data: formData
+      });
+      console.log(result.data);
+      setJson(result.data);
+
+      return result.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTable = async () => {
+    showPhoto(0);
+    setJson(await getConsulta("https://jsonplaceholder.typicode.com/posts"));
+    showTable(1);
+  };
+
+  const getPhotos = async () => {
+    showTable(0);
+    setJson(await getConsulta("https://jsonplaceholder.typicode.com/photos"));
+    showPhoto(1);
+  };
+
   return (
     <div className="App">
       <h1>Segundo Examen</h1>
       <h2>Barradas Cerna Adrián Antonio</h2>
-      <button> Tabla</button>
-      <button> Fotos</button>
-      <Table />
+      <button onClick={getTable}> Tabla</button>
+      <button onClick={getPhotos}> Fotos</button>
+      <Table show={table} data={json} />
+      <Table show={photo} data={json} />
     </div>
   );
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function Table(prop) {
+export default function Photo(prop) {
   const json = prop.data;
 
   return prop.show ? (
     <div className="container">
       <div className="row">
-        <div className="col-md-3"> userId</div>
         <div className="col-md-3"> id</div>
         <div className="col-md-3"> title</div>
-        <div className="col-md-3"> body</div>
+        <div className="col-md-3"> url</div>
+        <div className="col-md-3"> thumbnailUrl</div>
       </div>
       <div className="row">
         <ul>
           {json.map((s) => (
             <div className="row">
-              <div className="col-md-3"> {s.userId}</div>
               <div className="col-md-3"> {s.id}</div>
               <div className="col-md-3"> {s.title}</div>
-              <div className="col-md-3"> {s.body}</div>
+              <div className="col-md-3">
+                {" "}
+                <img src={require()} />
+                {s.url}
+              </div>
+              <div className="col-md-3"> {s.thumbnailUrl}</div>
             </div>
           ))}
         </ul>
